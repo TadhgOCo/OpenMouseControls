0d9433252e6e401b3a8b7ff2260dd61d94a77bec
Tadhg ÓC <tocomhraidhe@outlook.com>
2026-02-26 20:29:57 +0000

    Improved Install scripts and added pillow to requirments.txt

--- install.sh
+++ install.sh
@@ -1,6 +1,31 @@
-#!/bin/sh
-cp 99-mouse-hid.rules /etc/udev/rules.d/
-udevadm control --reload-rules
-udevadm trigger
+#!/bin/bash
 
-pip install -e .
+if [ "$EUID" -ne 0 ]; then 
+  echo "Please run the script as root (use sudo [script name])"
+  exit
+fi
+
+echo "Installing Permissions..."
+if [ -f "udev/99-mouse-hid.rules" ]; then
+    cp udev/99-mouse-hid.rules /etc/udev/rules.d/
+    udevadm control --reload-rules
+    udevadm trigger
+    echo "Permissions Installed!"
+else
+    echo "Error: udev rule file not found."
+fi
+
+read -p "Would you like to install the Python Package (Y/n): " isInstalling
+
+if [[ "$isInstalling" =~ ^[Yy]$ ]]; then
+    echo "Installing Python Package..."
+
+    if [ -f "requirments.txt" ]; then
+        python3 -m pip install -r requirments.txt
+    fi
+
+    python3 -m pip install -e .
+    echo "Finished Installing Python Package"
+fi
+
+echo "Done!"
--- udev/install-udev.sh
+++ udev/install-udev.sh
@@ -1,4 +1,18 @@
-#!/bin/sh
-cp "$1/udev/99-mouse-hid.rules" /etc/udev/rules.d/
-udevadm control --reload-rules
-udevadm trigger
+#!/bin/bash
+
+if [ "$EUID" -ne 0 ]; then 
+  echo "Please run the script as root (use sudo [script name])"
+  exit
+fi
+
+echo "Installing Permissions..."
+if [ -f "udev/99-mouse-hid.rules" ]; then
+    cp "$1/udev/99-mouse-hid.rules" /etc/udev/rules.d/
+    udevadm control --reload-rules
+    udevadm trigger
+    echo "Permissions Installed!"
+else
+    echo "Error: udev rule file not found."
+fi
+
+echo "Done!"
